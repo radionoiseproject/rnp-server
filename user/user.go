8fe88c91f5380ef773af9d9bc483d3bc47f6b1c1
Calvin Walton <calvin.walton@kepstin.ca>
2015-09-28 20:52:12 -0400

    Some partial websocket stuff in the sofar unused user.

--- user/user.go
+++ user/user.go
@@ -4,6 +4,7 @@
 package user
 
 import "github.com/radionoiseproject/rnp-server/interfaces"
+import "github.com/gorilla/websocket"
 
 type Message struct {
 }
@@ -11,9 +12,26 @@ type Message struct {
 type user struct {
 	id   string
 	name string
+	ws   *websocket.Conn
 	hub  *interfaces.Hub
 }
 
-func New(id string, name string, hub *interfaces.Hub) *user {
-	return &User{id: id, name: name, hub: hub}
+func New(ws *websocket.Conn, hub *interfaces.Hub) *user {
+	return &user{ws: ws, hub: hub}
+}
+
+// interfaces.User
+
+func (u *user) Send(msg interfaces.Message) {
+}
+
+func (u *user) Id() (string, error) {
+	err := nil
+	if u.id == nil {
+		err = Error("User id is not set")
+	}
+	return u.id, err
+}
+
+func (u *user) ForceDisconnect() {
 }
